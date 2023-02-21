7e82b61aed7b65d39c9957df0add271380c0dc5c
info-mic-info <jobsocial94@gmail.com>
2023-02-21 21:05:32 +0100

    seeder inizializzato e creato

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
