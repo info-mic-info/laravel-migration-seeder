9d4553277e317be351eb12cc451e7fc5e1ff9bce
info-mic-info <jobsocial94@gmail.com>
2023-02-21 20:58:14 +0100

    eseguite migration table

--- database/migrations/2023_02_21_193645_create_trains_table.php
+++ database/migrations/2023_02_21_193645_create_trains_table.php
@@ -19,12 +19,12 @@ return new class extends Migration
             $table->string('Azienda', 50);
             $table->string('Stazione_di_partenza', 50);
             $table->string('Stazione_di_arrivo', 50);
-            $table->string('Orario_di_partenza', 50);
-            $table->string('Orario_di_arrivo', 50);
-            $table->string('Codice_Treno', 50);
-            $table->string('Numero_Carrozze', 50);
-            $table->string('In_orario', 50);
-            $table->string('Cancellato', 50);
+            $table->time('Orario_di_partenza');
+            $table->time('Orario_di_arrivo');
+            $table->string('Codice_Treno', 50); 
+            $table->smallInteger('Numero_Carrozze')->nullable();
+            $table->boolean('In_orario');
+            $table->boolean('Cancellato');
           
         });
     }
